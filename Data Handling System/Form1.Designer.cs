8e409b0e2d7d99999e0b108efa341cc5297d2ec6
DeepeshKC <you@example.com>
2018-11-13 22:39:10 +0545

    Data Read and Data Header Read

--- Data Handling System/Form1.Designer.cs
+++ Data Handling System/Form1.Designer.cs
@@ -32,7 +32,16 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblSMode = new System.Windows.Forms.Label();
+            this.lblMonitor = new System.Windows.Forms.Label();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.lblStartTime = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,17 +73,97 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(29, 211);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(41, 13);
+            this.lblWeight.TabIndex = 13;
+            this.lblWeight.Text = "Weight";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(29, 154);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 12;
+            this.lblDate.Text = "Date";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(29, 179);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 10;
+            this.lblLength.Text = "Length";
+            // 
+            // lblSMode
+            // 
+            this.lblSMode.AutoSize = true;
+            this.lblSMode.Location = new System.Drawing.Point(29, 131);
+            this.lblSMode.Name = "lblSMode";
+            this.lblSMode.Size = new System.Drawing.Size(41, 13);
+            this.lblSMode.TabIndex = 11;
+            this.lblSMode.Text = "SMode";
+            // 
+            // lblMonitor
+            // 
+            this.lblMonitor.AutoSize = true;
+            this.lblMonitor.Location = new System.Drawing.Point(29, 107);
+            this.lblMonitor.Name = "lblMonitor";
+            this.lblMonitor.Size = new System.Drawing.Size(42, 13);
+            this.lblMonitor.TabIndex = 9;
+            this.lblMonitor.Text = "Monitor";
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(32, 84);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(42, 13);
+            this.lblInterval.TabIndex = 8;
+            this.lblInterval.Text = "Interval";
+            // 
+            // lblStartTime
+            // 
+            this.lblStartTime.AutoSize = true;
+            this.lblStartTime.Location = new System.Drawing.Point(29, 53);
+            this.lblStartTime.Name = "lblStartTime";
+            this.lblStartTime.Size = new System.Drawing.Size(55, 13);
+            this.lblStartTime.TabIndex = 7;
+            this.lblStartTime.Text = "Start Time";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(322, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(447, 410);
+            this.dataGridView1.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.lblSMode);
+            this.Controls.Add(this.lblMonitor);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.lblStartTime);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +175,14 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblSMode;
+        private System.Windows.Forms.Label lblMonitor;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Label lblStartTime;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Data Handling System/Form1.cs
+++ Data Handling System/Form1.cs
@@ -7,28 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Data_Handling_System
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> _hrmData = new Dictionary<string, string>();
+        private Dictionary<string, string> _param = new Dictionary<string, string>();
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private static string[] SplitString(string text)
+        {
+            var splitString = new string[] {"[Params]","[Note]","[IntTimes]", "[IntNotes]",
+                "[ExtraData]", "[LapNames]", "[Summary-123]",
+                "[Summary-TH]", "[HRZones]", "[SwapTimes]", "[Trip]", "[HRData]"};
+
+            var splittedText = text.Split(splitString, StringSplitOptions.RemoveEmptyEntries);
+
+            return splittedText;
+        }
+
+        private static string[] SplitStringByEnter(string text)
+        {
+            return text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        //to open the file
         private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = openFileDialog1.ShowDialog();
 
-            if(DialogResult == DialogResult.OK )
+            if (dialogResult == DialogResult.OK)
             {
-                string file = openFileDialog1.FileName;
-                string text = System.IO.File.ReadAllText(file);
+                _param = new Dictionary<string, string>();
+                string text = File.ReadAllText(openFileDialog1.FileName);
+                var splittedString = SplitString(text);
 
-                Console.WriteLine(text);
+                var splittedparamsData = SplitStringByEnter(splittedString[0]);
+
+                foreach (var data in splittedparamsData)
+                {
+                    if (data != "\r")
+                    {
+                        string[] parts = data.Split(new[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                        _param.Add(parts[0], parts[1]);
+                    }
+                }
+
+                lblStartTime.Text = lblStartTime.Text + "= " + _param["StartTime"];
+                lblInterval.Text = lblInterval.Text + "= " + _param["Interval"];
+                lblMonitor.Text = lblMonitor.Text + "= " + _param["Monitor"];
+                lblSMode.Text = lblSMode.Text + "= " + _param["SMode"];
+                lblDate.Text = lblDate.Text + "= " + _param["Date"];
+                lblLength.Text = lblLength.Text + "= " + _param["Length"];
+                lblWeight.Text = lblWeight.Text + "= " + _param["Weight"];
             }
-
         }
     }
 }
