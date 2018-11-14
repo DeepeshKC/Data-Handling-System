f0149ebc0db2fa33f0a209f010ea1eec3b06848e
DeepeshKC <you@example.com>
2018-11-14 08:57:40 +0545

    Data displayed in grid

--- Data Handling System/Form1.cs
+++ Data Handling System/Form1.cs
@@ -12,12 +12,13 @@ namespace Data_Handling_System
 {
     public partial class Form1 : Form
     {
-        private Dictionary<string, string> _hrmData = new Dictionary<string, string>();
+        private Dictionary<string, List<string>> _hrData = new Dictionary<string, List<string>>();
         private Dictionary<string, string> _param = new Dictionary<string, string>();
 
         public Form1()
         {
             InitializeComponent();
+            InitGrid();
         }
 
         private static string[] SplitString(string text)
@@ -36,20 +37,26 @@ namespace Data_Handling_System
             return text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
         }
 
+        private string[] SplitStringBySpace(string text)
+        {
+            var formattedText = string.Join(" ", text.Split().Where(x => x != ""));
+            return formattedText.Split(' ');
+        }
+
         //to open the file
         private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = openFileDialog1.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
             {
                 _param = new Dictionary<string, string>();
+                _hrData = new Dictionary<string, List<string>>();
                 string text = File.ReadAllText(openFileDialog1.FileName);
                 var splittedString = SplitString(text);
 
-                var splittedparamsData = SplitStringByEnter(splittedString[0]);
+                var splittedParamsData = SplitStringByEnter(splittedString[0]);
 
-                foreach (var data in splittedparamsData)
+                foreach (var data in splittedParamsData)
                 {
                     if (data != "\r")
                     {
@@ -58,6 +65,7 @@ namespace Data_Handling_System
                     }
                 }
 
+
                 lblStartTime.Text = lblStartTime.Text + "= " + _param["StartTime"];
                 lblInterval.Text = lblInterval.Text + "= " + _param["Interval"];
                 lblMonitor.Text = lblMonitor.Text + "= " + _param["Monitor"];
@@ -65,7 +73,45 @@ namespace Data_Handling_System
                 lblDate.Text = lblDate.Text + "= " + _param["Date"];
                 lblLength.Text = lblLength.Text + "= " + _param["Length"];
                 lblWeight.Text = lblWeight.Text + "= " + _param["Weight"];
+
+                List<string> cadence = new List<string>();
+                List<string> altitude = new List<string>();
+                List<string> heartRate = new List<string>();
+                List<string> watt = new List<string>();
+
+                //adding data for datagrid
+                var splittedHrData = SplitStringByEnter(splittedString[11]);
+                foreach (var data in splittedHrData)
+                {
+                    var value = SplitStringBySpace(data);
+
+                    if (value.Length >= 4)
+                    {
+                        cadence.Add(value[0]);
+                        altitude.Add(value[1]);
+                        heartRate.Add(value[2]);
+                        watt.Add(value[3]);
+
+                        string[] hrData = new string[] { value[0], value[1], value[2], value[3] };
+                        dataGridView1.Rows.Add(hrData);
+                    }
+                }
+
+                _hrData.Add("cadence", cadence);
+                _hrData.Add("altitude", altitude);
+                _hrData.Add("heartRate", heartRate);
+                _hrData.Add("watt", watt);
+                
             }
+        }
+
+        private void InitGrid()
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "Cadence";
+            dataGridView1.Columns[1].Name = "Altitude";
+            dataGridView1.Columns[2].Name = "Heart rate";
+            dataGridView1.Columns[3].Name = "Power in watts";
         }
     }
 }
